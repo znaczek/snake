8deea676e0d6d9e693e8f6d97f4aafac6c0a7364
Wojciech Znaczko <wojciech.znaczko@gmail.com>
2019-02-14 23:04:29 +0100

    release

---
+++ sw.js
@@ -0,0 +1,56 @@
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+const PRECACHE_URLS = [
+    'index.html',
+    './',
+    'bundle.js',
+    'android-chrome-144x144.png',
+    'apple-touch-icon.png',
+    'favicon.ico',
+    'favicon-16x16.png',
+    'favicon-32x32.png',
+    'mstile-150x150.png',
+    'GitHub-Mark-32px.png',
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(PRECACHE)
+            .then(cache => cache.addAll(PRECACHE_URLS))
+            .then(self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+        }).then(cachesToDelete => {
+            return Promise.all(cachesToDelete.map(cacheToDelete => {
+                return caches.delete(cacheToDelete);
+            }));
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            caches.match(event.request).then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return caches.open(RUNTIME).then(cache => {
+                    return fetch(event.request).then(response => {
+                        return cache.put(event.request, response.clone()).then(() => {
+                            return response;
+                        });
+                    });
+                });
+            })
+        );
+    }
+});
